1def39c2f458bbb98c3daf520a1fcb53889a6049
fatih <109206915+fatihblockgain@users.noreply.github.com>
2023-02-20 09:11:00 +0300

    feat: beginning . . .

--- apps/web/app.vue
+++ apps/web/app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <AtomButton/>
+  <div class="main">
+    <div class="background">
+      <div class="patterns">
+        <img v-for="i in 3" :key="i" class="pattern" src="logo.svg" alt="logo">
+      </div>
+      <div class="noise" />
+    </div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<style lang="postcss">
+.main {
+  @apply w-full h-full;
+}
+</style>
--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-   components: {
-    dirs: [
-        '~/components',
-        '../../packages/ui/components'
-    ],
-   },
-   
+  css: ['~/assets/styles/main.pcss'],
+  components: {
+    dirs: ['~/components', '../../packages/ui/components'],
+  },
+  modules: ['@nuxtjs/tailwindcss','@nuxt/devtools','@vue-macros/nuxt'],
+  tailwindcss: {
+    configPath: '~/packages/ui/tailwind.config.js',
+  }
 })
